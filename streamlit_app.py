0932000385734ca888c1a48509125146c86adfea
Louie Cervantes <128763658+louiecervantes@users.noreply.github.com>
2024-02-16 15:06:09 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ def app():
     with st.echo(code_location='below'):
         if st.button('Load the dataset'):
             df = pd.read_csv('titanic.csv', header=0)
-            st.dataframe(df, use_container_width=True)  
+            # st.dataframe(df, use_container_width=True)  
 
             #load the data and the labels
             X = df.values[:,0:-1]
@@ -42,7 +42,8 @@ def app():
 
             df = labeltonumeric(df, 'Sex')
             df = labeltonumeric(df, 'Embarked')
-    
+            st.dataframe(df, use_container_width=True)  
+            
             # Split the dataset into training and testing sets
             X_train, X_test, y_train, y_test = train_test_split(X, y, \
                 test_size=0.2, random_state=42)
